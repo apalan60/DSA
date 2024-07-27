b1b2739f0f85c8f0d8a49582d35fbf3f62905d24
apalan60 <apalan60@gmail.com>
2024-07-27 14:47:15 +0800

    QuickFind

---
+++ DSA/QuickFind.cs
@@ -0,0 +1,20 @@
+﻿namespace DSA;
+
+public class QuickFind
+{
+    private int[] _id;
+
+    public void Union(int p , int q)
+    {
+        int pId = _id[p];
+        int qId = _id[q];
+
+        for (int i = 0; i < _id.Length; i++)
+        {
+            if (_id[i] == pId)
+            {
+                _id[i] = qId;
+            }
+        }
+    }
+}
