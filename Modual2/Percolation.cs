6e86a3777ef7b5c3f5da6f0eec02aabd481cb261
apalan60 <apalan60@gmail.com>
2024-07-28 12:38:52 +0800

    initial Percolation

---
+++ Modual2/Percolation.cs
@@ -0,0 +1,34 @@
+﻿namespace Modual2;
+
+public class Percolation
+{
+    // creates n-by-n grid, with all sites initially blocked
+    public Percolation(int n)
+    {
+    }
+
+    // opens the site (row, col) if it is not open already
+    public void Open(int row, int col)
+    {
+    }
+
+    // is the site (row, col) open?
+    public bool IsOpen(int row, int col)
+    {
+    }
+
+    // is the site (row, col) full?
+    public bool IsFull(int row, int col)
+    {
+    }
+
+    // returns the number of open sites
+    public int numberOfOpenSites()
+    {
+    }
+
+    // does the system percolate?
+    public bool percolates()
+    {
+    }
+}
