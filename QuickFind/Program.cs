bbc97316f904e204033efaa5a6339faf67e05e8f
apalan60 <apalan60@gmail.com>
2024-07-30 23:08:11 +0800

    QuickFind
    - Eager Approach
    
    add note

---
+++ QuickFind/Program.cs
@@ -0,0 +1,21 @@
+﻿using QuickFind_Eager;
+
+//有一組數列int[]
+//index為編號, 可以Union使兩個點的value相等
+//value相等代表該編號的點相連
+//實作連結點，以及判斷點是否相連的方法
+
+
+
+QuickFind quickFind = new QuickFind(10);
+
+Console.WriteLine(quickFind.Connected(1,2));
+Console.WriteLine(quickFind.Connected(1,3));
+Console.WriteLine(quickFind.Connected(1,4));
+
+
+Console.WriteLine("---------Union----------");
+quickFind.Union(1, 2);
+quickFind.Union(2, 3);
+Console.WriteLine(quickFind.Connected(1,3));
+Console.WriteLine(quickFind.Connected(1,4));
---
+++ QuickFind/QuickFind.cs
@@ -0,0 +1,30 @@
+﻿namespace QuickFind_Eager;
+
+/// <summary>
+/// 效能: 初始化: n, Union: n  => 若有n個數列，要耗費 n²的時間
+/// </summary>
+public class QuickFind
+{
+    private readonly int[] _id;
+
+    public QuickFind(int n)
+    {
+        _id = new int[n];
+        for (int i = 0; i < n; i++)
+        {
+            _id[i] = i;
+        }
+    }
+
+    public bool Connected(int p, int q) => _id[p] == _id[q];
+
+    public void Union(int p, int q)
+    {
+        int pId = _id[p];
+        int qId = _id[q];
+        for (int i = 0; i < _id.Length; i++)
+        {
+            if (_id[i] == pId) _id[i] = qId;
+        }
+    }
+}
