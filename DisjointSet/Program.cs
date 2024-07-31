d8f59a0bb6d90734b141feb7e6a61cdf84fcb42f
apalan60 <apalan60@gmail.com>
2024-07-31 22:19:45 +0800

    Quick Union

---
+++ DisjointSet/Program.cs
@@ -0,0 +1,39 @@
+﻿using DisjointSet;
+
+//有一組數列int[]
+//index為編號, 可以Union使兩個點的value相等
+//value相等代表該編號的點相連
+//實作連結點，以及判斷點是否相連的方法
+
+
+
+Console.WriteLine("---------Quick Find----------");
+QuickFind quickFind = new QuickFind(10);
+
+Console.WriteLine(quickFind.Connected(1,2));
+Console.WriteLine(quickFind.Connected(1,3));
+Console.WriteLine(quickFind.Connected(1,4));
+
+
+quickFind.Union(1, 2);
+quickFind.Union(2, 3);
+Console.WriteLine(quickFind.Connected(1,3));
+Console.WriteLine(quickFind.Connected(1,4));
+
+
+
+
+Console.WriteLine("---------Quick Union----------");
+QuickUnion qu = new QuickUnion(10);
+
+Console.WriteLine(qu.Connected(1,2));
+Console.WriteLine(qu.Connected(1,3));
+Console.WriteLine(qu.Connected(1,4));
+
+qu.Union(1, 2);
+qu.Union(2, 3);
+qu.Union(6, 1);
+
+Console.WriteLine(qu.Connected(1,3));
+Console.WriteLine(qu.Connected(1,4));
+Console.WriteLine(qu.Connected(6,3));
---
+++ DisjointSet/QuickUnion.cs
@@ -0,0 +1,39 @@
+﻿namespace DisjointSet;
+
+/// <summary>
+/// Tree can get tall
+/// </summary>
+public class QuickUnion
+{
+    private readonly int[] _id;
+    
+    public QuickUnion(int n)  //O(n)
+    {
+        _id = new int[n];
+        for (int i = 0; i < n; i++)
+        {
+            _id[i] = i;
+        }
+    }
+
+    public bool Connected(int p, int q) => Root(p) == Root(q);  //O(n) in worst situation
+    
+    public void Union(int p , int q)  //O(n) in worst situation
+    {
+        int rootP = Root(p);
+        int rootQ = Root(q);
+
+        _id[rootP] = rootQ;
+    }
+    
+    private int Root(int i)
+    {
+        while (_id[i] != i)
+        {
+            i = _id[i];
+        }
+
+        return i;
+    }
+
+}
