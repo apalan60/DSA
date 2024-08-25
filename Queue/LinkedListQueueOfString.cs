c9cab25b4d5a94ab10264e822f8104c04ff36efe
apalan60 <apalan60@gmail.com>
2024-08-25 12:50:03 +0800

    LinkedListQueueOfString

---
+++ Queue/LinkedListQueueOfString.cs
@@ -0,0 +1,57 @@
+﻿namespace Queue;
+
+/// <summary>
+/// First in first out 
+/// </summary>
+public class LinkedListQueueOfString
+{
+    private Node? _first;
+    
+    private Node? _last;
+    
+    private class Node
+    {
+        internal string Item = null!;
+        internal Node? Next;
+    }
+
+    private bool IsEmpty() => _first == null;
+    
+    public void Enqueue(string item)
+    {
+        var oldLast = _last;
+        var last = new Node
+        {
+            Item = item,
+            Next = null
+        };
+        
+        if (IsEmpty())
+        {
+            _first = last;
+            _last = last;
+        }
+
+        else if (oldLast != null)
+        {
+            oldLast.Next = last;
+            _last = last;
+        }
+
+    }
+
+    public string? Dequeue()
+    { 
+        // Save item to return
+        string? first = _first?.Item;
+        
+        //Delete first node
+        _first = _first?.Next;
+        
+        //Check
+        if (IsEmpty()) 
+            _last = null;
+        
+        return first;
+    }
+}
---
+++ Queue/Program.cs
@@ -0,0 +1,15 @@
+﻿using Queue;
+
+var queue = new LinkedListQueueOfString();
+queue.Enqueue("A");
+queue.Enqueue("B");
+queue.Enqueue("C");
+queue.Enqueue("D");
+queue.Enqueue("E");
+
+
+Console.WriteLine(queue.Dequeue());
+Console.WriteLine(queue.Dequeue());
+Console.WriteLine(queue.Dequeue());
+Console.WriteLine(queue.Dequeue());
+Console.WriteLine(queue.Dequeue());
